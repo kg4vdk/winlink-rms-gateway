c3a1c8651e5c41abf7cfd3d0636b64cd622e8625
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-05-10 09:37:23 -0500

    Add files via upload

---
+++ winlink-rms-gateway-build.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+
+#########################
+
+# Change these variables for your use
+
+CALLSIGN=N0CALL #
+SSID=10 #CALLSIGN-NN
+LOCATOR=XXNNXX #6 character gridsquare
+WLNKPASS=********** #Super secret Winlink password
+FREQ=NNN.NNN #Frequency for the gateway
+NEWHOSTNAME=rms-gateway #Hostname for the machine
+
+#########################
+
+# Comment out CD-ROM repository
+sudo sed -i 's/^deb cdrom:/#&/' /etc/apt/sources.list
+
+# Add access to i386 packages
+sudo dpkg --add-architecture i386
+
+# Update package lists
+sudo apt update
+
+# Install packages
+sudo apt install -y direwolf zlib1g:i386 tmux tor
+
+# Add user to the "dialout" group
+sudo usermod -aG dialout $USER
+
+# Enable linger for user's services
+sudo loginctl enable-linger $USER
+
+# Remove extraneous directories from user's $HOME
+rm -rf $HOME/{Documents,Music,Pictures,Public,Templates,Videos}
+
+#########################
+
+# Digirig udev rule (creates /dev/digirig --> /dev/ttyUSBN)
+cat <<EOF > /tmp/digirig.rules
+SUBSYSTEM=="tty", GROUP="dialout", MODE="0660", ATTRS{product}=="CP2102N USB to UART Bridge Controller", SYMLINK+="digirig"
+EOF
+sudo cp /tmp/digirig.rules /etc/udev/rules.d/digirig.rules
+rm /tmp/digirig.rules
+
+#########################
+
+# Alsa configs to force digirig to Card #5 and use digirig-rx/digirig-tx
+cat <<EOF > /tmp/alsa-base.conf
+options snd-usb-audio index=5
+EOF
+sudo cp /tmp/alsa-base.conf /etc/modprobe.d/alsa-base.conf
+rm /tmp/alsa-base.conf
+
+cat <<EOF > /tmp/asound.conf
+pcm_slave.digirig {
+   pcm {
+      type hw
+      card 5
+   }
+   period_time 0
+   buffer_size 8192
+   rate 44100
+}
+
+pcm.digirig-dmix {
+   type dmix
+   ipc_key 2023041901
+   slave "digirig"
+   bindings.0 0
+}
+
+pcm.digirig-dsnoop {
+   type dsnoop
+   ipc_key 2023041902
+   slave "digirig"
+   bindings.0 0
+}
+
+pcm.digirig-rx {
+   type plug
+   slave.pcm "digirig-dsnoop"
+   hint.description "digirig RX audio plug"
+}
+
+pcm.digirig-tx {
+   type plug
+   slave.pcm "digirig-dmix"
+   hint.description "digirig TX audio plug"
+}
+EOF
+sudo cp /tmp/asound.conf /etc/asound.conf
+rm /tmp/asound.conf
+
+#########################
+
+# Direwolf
+mkdir -p $HOME/DIREWOLF
+cat <<EOF > $HOME/DIREWOLF/direwolf.conf
+MYCALL $CALLSIGN
+ADEVICE digirig-rx digirig-tx
+TXDELAY 50
+PTT /dev/digirig RTS
+EOF
+
+cat <<EOF > $HOME/DIREWOLF/start-direwolf.sh
+amixer -c 5 sset "Auto Gain Control" mute
+mkdir -p $HOME/DIREWOLF/logs
+direwolf -c $HOME/DIREWOLF/direwolf.conf -p -t 0 -T '%FT%T%Z' | tee $HOME/DIREWOLF/logs/direwolf.log
+EOF
+chmod +x $HOME/DIREWOLF/start-direwolf.sh
+
+#########################
+
+# LinBPQ
+mkdir -p $HOME/LINBPQ
+cd $HOME/LINBPQ
+rm linbpq
+wget -nv http://cantab.net/users/john.wiseman/Downloads/Beta/linbpq
+chmod +x $HOME/LINBPQ/linbpq
+rm -rf $HOME/LINBPQ/HTML
+mkdir -p $HOME/LINBPQ/HTML
+cd $HOME/LINBPQ/HTML
+wget -nv http://cantab.net/users/john.wiseman/Downloads/Beta/HTMLPages.zip
+unzip HTMLPages.zip
+rm HTMLPages.zip
+
+cat <<EOF > $HOME/LINBPQ/bpq32.cfg
+SIMPLE ; This sets many parameters to reasonable defaults
+
+LOCATOR=$LOCATOR ; Set to your Grid Square to send reports to the BPQ32 Node Map system
+NODECALL=$CALLSIGN
+
+INFOMSG:
+$CALLSIGN's RMS Gateway
+***
+
+PORT
+ ID=Telnet Server
+ DRIVER=TELNET
+ CONFIG
+ LOGGING=1
+ CMS=1 ; Enable CMS Gateway
+ CMSCALL=$CALLSIGN ; CMS Gateway Call for Secure CMS Access(normally same as NODECALL)
+ CMSPASS=$WLNKPASS ; Secure CMS Password
+ HTTPPORT=8073 ; Port used for Web Management
+ TCPPORT=8010 ; Port for Telnet Access
+ FBBPORT=8011 ; Not required, but allows monitoring using BPQTermTCP
+ MAXSESSIONS=10
+ CloseOnDisconnect=1 ; Close Telent Session when Node disconnects
+ USER=user,password,$CALLSIGN,"",SYSOP
+ 
+ ENDPORT
+ 
+ ; Add Radio Port(s) Here
+PORT
+ ID=1200 Baud $FREQ
+ TYPE=ASYNC
+ PROTOCOL=KISS
+ CHANNEL=A
+ COMPORT=/tmp/kisstnc
+ SPEED=9600
+ MAXFRAME=4
+ FRACK=8000
+ RESPTIME=1500
+ RETRIES=10
+ PACLEN=120
+ TXDELAY=300
+ SLOTTIME=100
+ PERSIST=64
+ 
+ WL2KREPORT PUBLIC, www.winlink.org, 8778, $CALLSIGN-$SSID, $LOCATOR, 00-23, $(echo $FREQ.000 | sed 's/\.//g'), PKT1200, 10, 100, 5, 0
+
+ENDPORT
+ 
+ APPLICATION 1,RMS,C 1 CMS,$CALLSIGN-$SSID
+EOF
+
+cat <<EOF > $HOME/LINBPQ/start-linbpq.sh
+cd $HOME/LINBPQ
+$HOME/LINBPQ/linbpq -c $HOME/LINBPQ -l $HOME/LINBPQ -d $HOME/LINBPQ
+EOF
+chmod +x $HOME/LINBPQ/start-linbpq.sh
+
+#########################
+
+# SystemD services for Direwolf and LinBPQ
+mkdir -p $HOME/.config/systemd/user
+
+cat <<EOF > $HOME/.config/systemd/user/direwolf.service
+[Unit]
+Description=Direwolf Service
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/bin/tmux new-session -d -s direwolf '$HOME/DIREWOLF/start-direwolf.sh'
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+
+cat <<EOF > $HOME/.config/systemd/user/linbpq.service
+[Unit]
+Description=LinBPQ Service
+After=direwolf.service
+
+[Service]
+Type=forking
+ExecStartPre=/usr/bin/sleep 15
+ExecStart=/usr/bin/tmux new-session -d -s linbpq '$HOME/LINBPQ/start-linbpq.sh'
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl --user enable direwolf.service
+systemctl --user enable linbpq.service
+
+#########################
+
+# Log rotation/cleanup
+cat <<EOF > /tmp/direwolf
+$HOME/DIREWOLF/logs/direwolf.log
+{
+  su $USER $USER
+  missingok
+  daily
+  copytruncate
+  rotate 30
+  dateext
+  dateformat _%Y%m%d
+}
+EOF
+sudo cp /tmp/direwolf /etc/logrotate.d/direwolf
+rm /tmp/direwolf
+
+cat <<EOF > $HOME/LINBPQ/log-cleanup.sh
+find $HOME/LINBPQ/logs/ -name *.log -mtime +30 -delete
+EOF
+chmod +x $HOME/LINBPQ/log-cleanup.sh
+echo "00 02 * * * $USER $HOME/LINBPQ/log-cleanup.sh" | sudo tee /etc/crontab
+
+#########################
+
+# Configure Tor for remote SSH access
+cat <<EOF > /tmp/torrc
+HiddenServiceDir /var/lib/tor/hidden_service/
+HiddenServicePort 22 127.0.0.1:22
+EOF
+sudo cp /tmp/torrc /etc/tor/torrc
+rm /tmp/torrc
+
+#########################
+
+# Disable GUI
+sudo systemctl set-default multi-user.target
+
+#########################
+
+# Set hostname
+OLDHOSTNAME=$(cat /etc/hostname)
+sudo hostnamectl set-hostname $NEWHOSTNAME
+sudo sed -i "s/$OLDHOSTNAME/$NEWHOSTNAME/" /etc/hosts
+
+#########################
+
+# Reboot
+sudo reboot
